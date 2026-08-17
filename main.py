18882eb8ce9a7ae95710666e42605283336f6216
mariaignatiyuk <mariaignatiyuk@gmail.com>
2026-08-17 23:00:54 +0300

    Add main.py and  .gitignore

---
+++ main.py
@@ -0,0 +1,36 @@
+class Geom:
+    name = 'Geom'
+
+
+    def set_coords(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x1 = x1
+        self.y1 = y1
+
+
+class Line(Geom):
+    def draw(self):
+        print("Малювання лінії")
+
+
+
+
+
+class Rect(Geom):
+    def draw(self):
+        print("Малювання прямокутника")
+
+
+
+
+
+#g = Geom()
+l = Line()
+r = Rect()
+l.set_coords(1, 1, 2, 3,)
+r.set_coords(2, 1, 3, 4)
+
+
+
+
