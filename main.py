36d81506f8b820e1834935848f75f7c0daabc823
mariaignatiyuk <mariaignatiyuk@gmail.com>
2026-08-20 22:34:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ class Geom:
     def set_coords(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
-        self.x1 = x1
-        self.y1 = y1
+        self.x1 = x2
+        self.y1 = y2
+        self.draw()
 
 
 class Line(Geom):
@@ -30,6 +31,8 @@ l = Line()
 r = Rect()
 l.set_coords(1, 1, 2, 3,)
 r.set_coords(2, 1, 3, 4)
+#print(l.__dict__)
+#print(r.__dict__)
 
 
 
